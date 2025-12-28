61c274908731ea44e28833be1a1af9ec48925249
Piyush Aggarwal <101887335+piyush-0930@users.noreply.github.com>
2025-12-28 18:52:49 +0530

    Add PrincipalRegister route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Register from "./pages/Register";
+import PrincipalRegister from "./pages/PrincipalRegister"; // ⬅️ NEW
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,7 +20,11 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/register" element={<Register />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* NEW PRINCIPAL ROUTE */}
+          <Route path="/principal-register" element={<PrincipalRegister />} />
+
+          {/* CATCH-ALL */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
